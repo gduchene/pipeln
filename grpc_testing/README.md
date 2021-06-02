ed34a23a259a53398cdb3c4cddf4099e0a81723d
Grégoire Duchêne <gduchene@awhk.org>
2021-06-02 12:48:59 +0100

    Add gRPC tests
    Those tests live in a separate Go module so that people will not depend
    on gRPC libraries unintentionally.

---
+++ grpc_testing/README.md
@@ -0,0 +1,4 @@
+This module exists so that anyone using `pipeln` will not download all
+the gRPC dependencies required for this test.
+
+Remember to run `go generate` before `go test`!
---
+++ grpc_testing/grpc_test.go
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: CC0-1.0
+
+package grpc_testing
+
+//go:generate protoc --go_opt=paths=source_relative --go_out=plugins=grpc:. echo.proto
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.awhk.org/pipeln"
+)
+
+type impl struct{}
+
+var _ EchoServer = impl{}
+
+func (impl) Echo(_ context.Context, req *EchoRequest) (*EchoResponse, error) {
+	return &EchoResponse{Message: req.Message}, nil
+}
+
+func Test(t *testing.T) {
+	ln := pipeln.New("test-backend-name")
+
+	ret := make(chan error)
+	srv := grpc.NewServer()
+	RegisterEchoServer(srv, &impl{})
+	go func() { ret <- srv.Serve(ln) }()
+
+	opts := []grpc.DialOption{grpc.WithContextDialer(ln.DialContextAddr), grpc.WithInsecure()}
+	req := &EchoRequest{Message: "Hello World!"}
+
+	t.Run("OK", func(t *testing.T) {
+		conn, err := grpc.Dial("test-backend-name", opts...)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		client := NewEchoClient(conn)
+		resp, err := client.Echo(context.Background(), req)
+		require.NoError(t, err)
+		assert.Equal(t, req.Message, resp.Message)
+	})
+
+	t.Run("Address Mismatch", func(t *testing.T) {
+		conn, err := grpc.Dial("bad-backend-name", opts...)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		client := NewEchoClient(conn)
+		_, err = client.Echo(context.Background(), req)
+		require.Error(t, err)
+		st := status.Convert(err)
+		require.NotNil(t, st)
+		assert.Equal(t, codes.Unavailable, st.Code())
+		assert.Contains(t, st.Message(), "invalid argument")
+	})
+
+	srv.GracefulStop()
+	assert.NoError(t, <-ret)
+
+	t.Run("Remote Connection Closed", func(t *testing.T) {
+		conn, err := grpc.Dial("test-backend-name", opts...)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		client := NewEchoClient(conn)
+		_, err = client.Echo(context.Background(), req)
+		require.Error(t, err)
+		st := status.Convert(err)
+		require.NotNil(t, st)
+		assert.Equal(t, codes.Unavailable, st.Code())
+		assert.Contains(t, st.Message(), "connection refused")
+	})
+}
